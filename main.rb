a1e71c47ce5484ffb36de65cf9c159f724c2dd6d
Dean <72749793+drossano@users.noreply.github.com>
2022-04-04 22:52:59 -0400

    Delete old code

--- main.rb
+++ main.rb
@@ -24,36 +24,8 @@ class Cipher
   def num_to_string(array)
     array.map { |n| n.chr}.join
   end
-  
+
   def cipher(text, shift)
     num_to_string(shift_arr(string_to_arr(text), shift))
-  end
-
-  def caesar_cipher (text, shift)
-    text_num = text.bytes
-    shfited_text_num = text_num.map do |n|
-      if n.between?(97,122)
-        shifted_number = n + shift
-        if shifted_number > 122
-          shifted_number -= 26
-        elsif shifted_number < 97
-          shifted_number += 26
-        else
-          shifted_number
-        end
-      elsif n.between?(65, 90)
-        shifted_number = n + shift
-        if shifted_number > 90
-          shifted_number -= 26
-        elsif shifted_number < 65
-          shifted_number += 26
-        else
-          shifted_number
-        end
-      else
-        n
-      end
-    end
-    puts shfited_text_num.map { |shifted_number| shifted_number.chr}.join
   end
 end
